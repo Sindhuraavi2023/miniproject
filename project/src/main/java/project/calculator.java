1b0e9ad731f50e7edd88f9b2a260cdcfed524073
chandanagc15 <chandanareddy1551@gmail.com>
2026-01-28 11:59:47 +0530

    added three Numers

--- project/src/main/java/project/calculator.java
+++ project/src/main/java/project/calculator.java
@@ -1,45 +1,50 @@
 package project;
 import java.util.Scanner;
 public class calculator {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+        
+        double num1, num2, num3, result = 0;
+        char operator;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter first number:");
-        double num1 = scanner.nextDouble();
+        
+        System.out.println("Enter three numbers:");
+        num1 = scanner.nextDouble();
+        num2 = scanner.nextDouble();
+        num3 = scanner.nextDouble();
 
-        System.out.println("Enter second number:");
-        double num2 = scanner.nextDouble();
+        
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = scanner.next().charAt(0);
 
-        System.out.println("Choose an operation (+, -, *, /):");
-        char operator = scanner.next().charAt(0);
-
-        double result;
-
+        
         switch (operator) {
             case '+':
-                result = num1 + num2;
+                result = num1 + num2 + num3;
                 break;
             case '-':
-                result = num1 - num2;
+                result = num1 - num2 - num3;
                 break;
             case '*':
-                result = num1 * num2;
+                result = num1 * num2 * num3;
                 break;
             case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error! Division by zero.");
-                    return;
+                if (num2 == 0 || num3 == 0) {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    scanner.close();
+                    return; 
                 }
+                result = num1 / num2 / num3;
                 break;
             default:
-                System.out.println("Invalid operator!");
-                return;
+                System.out.println("Error: Invalid operator entered.");
+                scanner.close();
+                return; 
         }
 
-        System.out.println("Result: " + result);
+        
+        System.out.println("\nThe result is: " + result);
         scanner.close();
     }
 }
-
+        
